d8b6308da2032de19e83dfd4e89a931dba3c1c22
Denis <web_life@inbox.ru>
2023-10-02 22:41:52 +0300

    git init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-main>
+      <ContentBlock/>
+    </v-main>
+
+    <v-footer app absolute fixed>
+      <Footer />
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import ContentBlock from './components/ContentBlock';
+import Footer from './components/Footer';
+
+export default {
+  name: 'App',
+  components: {
+    ContentBlock,
+    Footer
+  },
+};
+</script>
---
+++ src/components/ContentBlock.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container>
+    <v-row
+      align="center"
+      justify="center"
+      class="ma-4"
+    >
+
+      <v-col cols="12">
+        <logo-view />
+      </v-col>
+      <v-col cols="12" md="4">
+        <filters-view />
+      </v-col>
+
+      <v-col cols="12" md="4">
+        <v-card max-width="450" class="mx-auto">
+          <users-view />
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import LogoView from "@/components/content-block/LogoView.vue";
+import FiltersView from "@/components/content-block/FiltersView/index.vue";
+import UsersView from "@/components/content-block/UsersView";
+  export default {
+    name: 'ContentBlock',
+    components: {
+      LogoView,
+      FiltersView,
+      UsersView
+    },
+  }
+</script>
